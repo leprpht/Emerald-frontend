516612b96e06982ff92a0888f868d645ec16ad6e
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-12-13 18:04:09 +0100

    Add delete functionality to Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,30 @@
 import type { Campaign } from '../../types/campaign';
 import './Card.css';
 import Keyword from '../Keyword/Keyword';
+import x from '/src/assets/x-icon.svg';
+import { deleteCampaign } from '../../services/api';
 
 interface CardProps {
   campaign: Campaign;
 }
 
 export default function Card(data: CardProps) {
+
+  function deleteThisCampaign() {
+    deleteCampaign(data.campaign.id);
+    window.location.reload();
+  }
+
   return (
     <div className="card">
-      <h3>{data.campaign.campaignName}</h3>
+      <div className="card-header">
+        <h3>{data.campaign.campaignName}</h3>
+        <a className='delete-button' onClick={deleteThisCampaign}>
+          <img src={x}></img>
+        </a>
+      </div>
       {data.campaign.keywords.map((keyword) => (
-        <Keyword text={keyword} />
+          <Keyword text={keyword} />
       ))}
       <p>Bid Amount: ${data.campaign.bidAmount}</p>
       <p>Campaign Fund: ${data.campaign.campaignFund}</p>
