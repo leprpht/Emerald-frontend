90acb71b2657f51ba170bc046bc7de646c7a2b51
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-12-14 14:39:16 +0100

    Implement navigation for AddButton and NavBar components

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router';
 import './AddButton.css'
 import plus from '/src/assets/plus-icon.svg'
 
 export default function AddButton() {
+  const nav = useNavigate();
+
   return (
-    <button className="add-button">
+    <button className="add-button" onClick={() => nav('/new-campaign')}>
       <img src={plus} alt="Add" />
     </button>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router';
 import './NavBar.css'
 
 export default function NavBar() {
+  const nav = useNavigate();
+
   return (
-    <nav className="navbar">
-      <h2>Emerald</h2>
+    <nav className="navbar" onClick={() => nav('/')}>
+      <button>
+        <h2>Emerald</h2>
+      </button>
     </nav>
   );
 }
